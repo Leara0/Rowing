1fbf4ae26b84bcffd342f501b257e6c37c01570b
Arael Seher <a.seher.85@gmail.com>
2026-01-14 15:13:14 -0800

    got stroke phases connected to api and working

--- Rowing.Application/UseCases/StrokePhaseUseCases/DTOs/StrokePhaseDto.cs
+++ Rowing.Application/UseCases/StrokePhaseUseCases/DTOs/StrokePhaseDto.cs
@@ -1,12 +1,19 @@
 namespace Rowing.Application.StrokePhase;
 using Rowing.Domain.Entities;
+using System.Text.Json.Serialization;
 
 public class StrokePhaseDto
 {
-    public int Id { get; init; }
-    public string Name { get; init; }
-    public string KeyFocus { get; init; }
+    [JsonPropertyName("phase_id")]
+    public int Id { get; set; }
+    [JsonPropertyName("name")]
+    public string Name { get; set; }
+    [JsonPropertyName("key_focus")]
+    public string KeyFocus { get; set; }
     
+    // Parameterless constructor for deserialization
+    public StrokePhaseDto() { }
+
     public StrokePhaseDto(StrokePhase model)
     {
         Id = model.Id;
